b752474cabb00c2faa0fae98eb37823d46e4ce6d
leannleann <choir.forever@yahoo.com>
2017-04-06 08:54:10 -0500

    did stuff
    enemy moves properly (not smoothly) now but the color change thing
    doesn't work at all

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -28,7 +28,8 @@ public class EnemyAI : MonoBehaviour
 	private bool navEngage = false;
 	private float coolTimer = 0f;
 	public float coolDownTime = 5f;
-	private bool pathPlacement = false;
+	private bool pathPlacement = true;
+	private bool attEngage = true;
 
 
 	// Use this for initialization
@@ -60,8 +61,19 @@ public class EnemyAI : MonoBehaviour
 
 		//Debug.Log(dist);
 
+		//recalculate leap path, unless already leaping
+		if(pathPlacement)
+		{
+			Debug.Log("YOU'RE MINE NOW!");
+			Vector3 newPos = Vector3.Lerp(transform.position, player.transform.position, 0.5f);
+			GetComponent<iTweenPath>().nodes[0] = transform.position;
+			GetComponent<iTweenPath>().nodes[1] = new Vector3(newPos.x, 3f, newPos.z);
+			GetComponent<iTweenPath>().nodes[2] = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+		}
+
+
 		//when it gets in range, start preparing to attack
-		if (dist > attackDistLow && dist <= attackDistHigh)
+		if (dist > attackDistLow && dist <= attackDistHigh && attEngage)
 		{
 			//agent.Stop();
 			agent.enabled = false;
@@ -71,13 +83,19 @@ public class EnemyAI : MonoBehaviour
 			 *with flashing color that gets faster as a timer goes down
 			 *Then call the actual attack function
 			 *that makes it leap at the player and then stay still for a short time*/
-			chargeUp = true;
 			curPos = transform.position;
-			//ChargeUp();
-			Attack();
+			ChargeUp();
 			//agent.Resume();
 		}
 
+		//when the leap is done, call activateNav
+		if (V3Equal(transform.position, GetComponent<iTweenPath>().nodes[2]))
+		{
+			Debug.Log("called it");
+			activateNav();
+		}
+
+		//reactivate navigation
 		if (navEngage)
 		{
 			agent.enabled = true;
@@ -126,18 +144,54 @@ public class EnemyAI : MonoBehaviour
 			}
 		}
 
+		//leap cooldown
 		if (theLoop)
 		{
 			coolTimer -= Time.deltaTime;
 			Debug.Log("Timer: " + coolTimer);
 			if (coolTimer <= 0)
 			{
+				attEngage = true;
 				navEngage = true;
 				coolTimer = coolDownTime;
 				theLoop = false;
 			}
 		}
 
+		if(chargeUp)
+		{
+			chargeTimer -= Time.deltaTime;
+			if (chargeTimer <= chargeTime && chargeTimer > firstThreshold)
+			{
+				//flash at a slow speed
+				//Debug.Log("Time: " + chargeTimer + " HRAA-");
+				ChargeUp("slow", 1.5f);
+				//Debug.Log("HRAA-");
+			}
+			else if (chargeTimer <= firstThreshold && chargeTimer > secondThreshold)
+			{
+				//flash at a faster speed
+				//iTween.ColorTo(gameObject, flashCol, 1f);
+				//Debug.Log("Time: " + chargeTimer + " -AAAAA-");
+				ChargeUp("faster", 1f);
+				//Debug.Log("-AAAAA-");
+			}
+			else if (chargeTimer <= secondThreshold && chargeTimer > 0)
+			{
+				//flash at fastest speed
+				//iTween.ColorTo(gameObject, flashCol, 0.5f);
+				//Debug.Log("Time: " + chargeTimer + " -AAAAAAAAAAAA");
+				ChargeUp("fastest", 0.5f);
+				//Debug.Log("-AAAAAAAAAAAA");
+			}
+			else if (chargeTimer <= 0)
+			{
+				chargeUp = false;
+				chargeTimer = chargeTime;
+				Attack();
+			}
+		}
+			
 	}
 
 	//prepare to attack
@@ -145,68 +199,56 @@ public class EnemyAI : MonoBehaviour
 	{
 		//function to "charge" a leap attack
 		//with flashing color that gets faster as a timer goes down
-		if(chargeUp)
+		chargeUp = true;
+	}
+
+	void ChargeUp(string howFast, float speed)
+	{
+		switch(howFast)
 		{
-			chargeTimer -= Time.deltaTime;
-			if (chargeTimer <= chargeTime && chargeTimer > firstThreshold)
-			{
-				//flash at a slow speed
-				//iTween.ColorTo(gameObject, flashCol, 2f);
-				Debug.Log("Time: " + chargeTimer + "HRAA-");
-				//Debug.Log("HRAA-");
-			}
-			else if (chargeTimer <= firstThreshold && chargeTimer > secondThreshold)
-			{
-				//flash at a faster speed
-				//iTween.ColorTo(gameObject, flashCol, 1f);
-				Debug.Log("Time: " + chargeTimer + "-AAAAA-");
-				//Debug.Log("-AAAAA-");
-			}
-			else if (chargeTimer <= secondThreshold && chargeTimer > 0)
-			{
-				//flash at fastest speed
-				//iTween.ColorTo(gameObject, flashCol, 0.5f);
-				Debug.Log("Time: " + chargeTimer + "-AAAAAAAAAAAA");
-				//Debug.Log("-AAAAAAAAAAAA");
-			}
-			else if (chargeTimer <= 0)
-			{
-				chargeUp = false;
-				chargeTimer = chargeTime;
-			}
+			case "slow":
+				Debug.Log("slow" + Time.time);
+				iTween.ColorTo(gameObject, flashCol, speed);
+				return;
+			case "faster":
+				Debug.Log("faster" + Time.time);
+				iTween.ColorTo(gameObject, flashCol, speed);
+				return;
+			case "fastest":
+				Debug.Log("fastest" + Time.time);
+				iTween.ColorTo(gameObject, flashCol, speed);
+				return;
 		}
 	}
 
 	//leap at the player
 	void Attack()
 	{
-		//leap at the player and then stay still for a short time
-		if (pathPlacement)
+		//if it hasn't leaped yet or isn't cooling down from a leap
+		if (!theLoop)
 		{
-			if (!theLoop)
-			{
-				Debug.Log("HYUP");
-				iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("jumper"), "time", 2f, "easetype", iTween.EaseType.linear, "oncomplete", "activateNav", "oncompletetarget", gameObject, "looptype", iTween.LoopType.none));
-			}
-			return;
+			Debug.Log("HYUP");
+			attEngage = false;
+			pathPlacement = false;
+			//iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("jumper"), "time", 2f, "easetype", iTween.EaseType.linear, "oncomplete", "activateNav", "oncompletetarget", gameObject, "looptype", iTween.LoopType.none));
+			iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath("jumper"), "time", 2f, "easetype", iTween.EaseType.linear, "looptype", iTween.LoopType.none));
 		}
-
-		Debug.Log("YOU'RE MINE NOW!");
-		Vector3 newPos = Vector3.Lerp(transform.position, player.transform.position, 0.5f);
-		GetComponent<iTweenPath>().nodes[0] = transform.position;
-		GetComponent<iTweenPath>().nodes[1] = new Vector3(newPos.x, 3f, newPos.z);
-		GetComponent<iTweenPath>().nodes[2] = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
-		//GetComponent<Rigidbody>().AddForce(gameObject.transform.forward * 1000f);
-
-		pathPlacement = true;
 
 	}
 
+	//activate leap cooldown and reactivates dynamic pathing
 	void activateNav()
 	{
 		Debug.Log("stahp");
 		theLoop = true;
-		pathPlacement = false;
+		pathPlacement = true;
+	}
+
+	//compare two vector3's
+	public bool V3Equal(Vector3 a, Vector3 b)
+	{
+		//Debug.Log("sqr mag: " + Vector3.SqrMagnitude(a - b));
+		return Vector3.SqrMagnitude(a - b) < 0.1;
 	}
 
 	/*void OnCollisionEnter(Collision col)
