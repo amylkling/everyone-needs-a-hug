602c66fcc3a7282fcac6fb15c5f182004c158308
leannleann <choir.forever@yahoo.com>
2017-04-27 20:04:14 -0500

    small fixes
    -changed the player hugging distance limit so it's easier to latch on to
    enemies
    -created trigger walls that should keep enemies from spawning outside
    the physical walls
    -made the main music quieter by default

---
+++ Assets/Scripts/WallTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallTrigger : MonoBehaviour {
+
+	void OnTriggerStay(Collider col)
+	{
+		if (col.gameObject.CompareTag("Enemy") || col.gameObject.CompareTag("Incapacitated"))
+		{
+			GameObject player = GameObject.FindWithTag("Player");
+			Vector3 newPos = new Vector3(player.transform.position.x + 0.5f, col.transform.position.y, player.transform.position.z + 0.5f);
+			col.transform.position = newPos;
+		}
+	}
+}
